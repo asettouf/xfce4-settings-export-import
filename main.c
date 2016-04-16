bd08899c6ac49a65d3b7ee8901fc3a7444d7f933
Adonis Settouf <adonis.settouf@gmail.com>
2016-04-16 21:13:10 +0000

    First commit

---
+++ main.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include"export.h"
+
+
+//check following file for keyboard shortcuts .config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml  
+
+//check this folder for most config file (where is window manager config files???) .config/xfce4/xfconf/xfce-perchannel-xml/
+int main(int argc, char *args){
+	
+
+	return 0;
+}
